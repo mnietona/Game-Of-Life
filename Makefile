6ff5a192f751bcf63248081d55b17c0a363e020c
matias <matiasnieto1419@gmail.com>
2024-02-13 20:55:43 +0100

    Modification MVC pour Welcome

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 .DEFAULT_GOAL := main
 
 main:
-	cd src && python3 main.py
+	cd src && python3 application.py
 	find . -type d -name __pycache__ -exec rm -rf {} +
 
 	
--- src/controller/welcome_controller.py
+++ src/controller/welcome_controller.py
@@ -1,9 +1,26 @@
-import pygame_widgets
+from view.welcome_view import WelcomeView
 
 class WelcomeController:
-    def __init__(self, view):
-        self.view = view
+    def __init__(self, app):
+        self.app = app
+        self.view = WelcomeView(app.screen)
 
-    def handle_event(self, events):
-        # Mettre à jour les widgets avec les événements
-        pygame_widgets.update(events)
+    def activate(self):
+        # Réinitialise l'état de la vue si nécessaire
+        self.view.reset_start_clicked()
+
+    def handle_event(self, event):
+        # Gère les événements et les transmet à la vue
+        self.view.handle_event(event)
+        # Vérifie si le bouton de démarrage a été cliqué
+        if self.view.start_clicked:
+            self.start_game()
+            self.view.reset_start_clicked()
+
+    def start_game(self):
+        # Logique pour démarrer le jeu, par exemple changer de vue
+        self.app.switch_controller("grid")
+
+    def render(self):
+        # Dessine la vue
+        self.view.render()

52f7f04bc91d324fc0363919009190c946fed201
matias <matiasnieto1419@gmail.com>
2024-02-15 17:01:55 +0100

    MVC Parfait

--- src/model/flora.py
+++ src/model/flora.py
@@ -4,10 +4,16 @@ class Plant:
 
     def update(self):
         pass
-
+    
+    def get_info(self):
+        return self.type
+    
 class Carrot:
     def __init__(self, energy=50):
         self.type = "Carrot"
         self.energy = energy
     def update(self):
         pass
+
+    def get_info(self):
+        return f"{self.type} energy: {self.energy}"

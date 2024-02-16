f942e1970becdb02ef58852ffa6df08d9af1e170
matias <matiasnieto1419@gmail.com>
2024-02-16 11:16:05 +0100

    Ajout reproduction

--- src/model/rabbit.py
+++ src/model/rabbit.py
@@ -1,3 +1,4 @@
+import random
 from src.constants import *
 from model.fauna import Fauna
 from model.flora import Carrot
@@ -9,6 +10,12 @@ class Rabbit(Fauna):
     
     def update(self, i, j, grid):
         super().update(i, j, grid)
+        
+        if self.health_level >= RABBIT_HEALTH_REPRODUCTION and random.random() < RABBIT_REPRODUCTION_RATE:
+            self.reproduce(grid)
+        
+    def reproduce(self, grid):
+        grid.add_entities(Rabbit, 1)
     
     @property
     def color(self):

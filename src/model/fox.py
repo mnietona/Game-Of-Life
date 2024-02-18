a4ba591aa6409b243de217049663d01afe581291
matias <matiasnieto1419@gmail.com>
2024-02-18 21:44:45 +0100

    Reafctore Faune et Flore

--- src/model/fox.py
+++ src/model/fox.py
@@ -5,18 +5,15 @@ from model.fauna import Fauna
 
 
 class Fox(Fauna):
-    reproduction_rate = FOX_REPRODUCTION_RATE
-    health_reproduction = FOX_HEALTH_REPRODUCTION
     
     def __init__(self, smart_level = 1):
-        super().__init__(FOX_HEALTH, FOX_RADIUS)
+        super().__init__(FOX_HEALTH, FOX_RADIUS, FOX_HEALTH_REPRODUCTION, FOX_REPRODUCTION_RATE)
         self.target_type = "Rabbit"
-        self.base_radius = FOX_RADIUS
         self.smart_level = smart_level
         self.adjust_radius_based_on_intelligence()
     
     def adjust_radius_based_on_intelligence(self):
-        self.radius = self.base_radius * self.smart_level
+        self.radius = FOX_RADIUS * self.smart_level
     
     def set_smart_level(self, smart_level):
         self.smart_level = smart_level
@@ -24,12 +21,7 @@ class Fox(Fauna):
     
     def update(self, i, j, grid):
         super().update(i, j, grid)
-        
-        if self.health_level >= self.health_reproduction and random.random() < self.reproduction_rate:
-            self.reproduce(grid)
-        
-    def reproduce(self, grid):
-        grid.add_entities(Fox, 1)
+        self.try_reproduce(grid)
 
     @property
     def color(self):
--- src/model/rabbit.py
+++ src/model/rabbit.py
@@ -3,61 +3,60 @@ from constants import *
 from model.fauna import Fauna
 
 class Rabbit(Fauna):
-    reproduction_rate = RABBIT_REPRODUCTION_RATE
-    health_reproduction = RABBIT_HEALTH_REPRODUCTION
 
     def __init__(self, smart_level = 1):
-        super().__init__(RABBIT_HEALTH, RABBIT_RADIUS) 
+        super().__init__(RABBIT_HEALTH, RABBIT_RADIUS, RABBIT_HEALTH_REPRODUCTION, RABBIT_REPRODUCTION_RATE)
         self.target_type = "Carrot"
         self.predator_type = "Fox"
-        self.base_radius = RABBIT_RADIUS
         self.smart_level = smart_level
         self.adjust_radius_based_on_intelligence()
         
     def adjust_radius_based_on_intelligence(self):
-        self.radius = self.base_radius * self.smart_level
+        self.radius = RABBIT_RADIUS * self.smart_level
         
     def set_smart_level(self, smart_level):
         self.smart_level = smart_level
         self.adjust_radius_based_on_intelligence()   
         
     def update(self, i, j, grid):
-        if self.smart_level > 2:
-            position = (i, j)
-            new_position = position
-            food_position = grid.find_nearest_target(position, self.radius, self.target_type)
-            predator_position = grid.find_nearest_target(position, self.radius, self.predator_type)
-            
-            if food_position and predator_position:
-                predator_distance = grid.calculate_distance(position, predator_position)
-                food_distance = grid.calculate_distance(position, food_position)
-                
-                if predator_distance < food_distance:
-                    new_position = self.flee(position, predator_position, grid)
-                else:
-                    new_position = self.move_towards(position, food_position, grid)
-            elif predator_position:
-                new_position = self.flee(position, predator_position, grid)
-            elif food_position:
-                new_position = self.move_towards(position, food_position, grid)
-            else:
-                new_position = self.move_randomly(i, j, grid)
-            grid.update_entity_position(position, new_position)
+        if self.smart_level > 1:
+            self.intelligent_behavior(i, j, grid)
         else:
             super().update(i, j, grid)
 
-        if self.health_level >= self.health_reproduction and random.random() < self.reproduction_rate:
-            self.reproduce(grid)
+        self.try_reproduce(grid)
+    
+    def intelligent_behavior(self, i, j, grid):
+        position = (i, j)
+        food_position = grid.find_nearest_target(position, self.radius, self.target_type)
+        predator_position = grid.find_nearest_target(position, self.radius, self.predator_type)
+
+        new_position = self.decide_action(position, food_position, predator_position, grid)
+        grid.update_entity_position(position, new_position)
+
+    def decide_action(self, position, food_position, predator_position, grid):
+        if predator_position and food_position:
+            return self.evaluate_threat_and_food(position, food_position, predator_position, grid)
+        elif predator_position:
+            return self.flee(position, predator_position, grid)
+        elif food_position:
+            return self.move_towards(position, food_position, grid)
+        else:
+            return self.move_randomly(position[0], position[1], grid)
+
+    def evaluate_threat_and_food(self, position, food_position, predator_position, grid):
+        predator_distance = self.calculate_distance(position, predator_position)
+        food_distance = self.calculate_distance(position, food_position)
+
+        if food_distance < predator_distance:
+            return self.move_towards(position, food_position, grid)
+        else:
+            return self.flee(position, predator_position, grid)
 
         
     def flee(self,current_position, predator_position, grid):
         return self.move_towards(current_position, predator_position, grid, flee=True)
         
-        
-        
-    def reproduce(self, grid):
-        grid.add_entities(Rabbit, 1)
-    
     @property
     def color(self):
         return WHITE

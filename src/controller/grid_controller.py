7a351595a0f687ae3a047ebbf64d42388a73258b
matias <matiasnieto1419@gmail.com>
2024-02-18 20:33:11 +0100

    Nettoyage Grid

--- src/controller/grid_controller.py
+++ src/controller/grid_controller.py
@@ -12,30 +12,35 @@ class GridController:
     def activate(self):
         if self.view is None:
             self.view = GridView(self.app.screen, self.model)
-        self.view.reset_button_clicks() 
+        self.view.reset_click_states() 
+        self.view.init_values()
         self.view.show_widgets()  
 
     def handle_event(self, event):
         if event.type == pygame.MOUSEBUTTONDOWN:
-            x, y = event.pos
-            i, j = self.get_cell_indices(x, y)
-            if self.is_valid_cell(i, j):
-                self.view.selected_cell = (i, j)
-        
+            self.handle_mouse_click(event.pos)
+            
         self.view.handle_event(event)  
-
+        self.update_view_from_model()
+        
+    def handle_mouse_click(self, position):
+        i, j = self.get_cell_indices(position[0], position[1])
+        if self.is_valid_cell(i, j):
+            self.view.selected_cell = (i, j)
+    
+    def update_view_from_model(self):
         if self.view.back_clicked:
             self.view.hide_widgets()
             self.app.switch_controller("welcome")
-            self.view.reset_button_clicks()  
+            self.view.reset_click_states() 
         elif self.view.pause_play_clicked:
             self.toggle_pause() 
             self.view.update_buttons_based_on_pause_state(self.paused)
-            self.view.reset_button_clicks()
+            self.view.reset_click_states() 
         elif self.view.next_step_clicked:
             self.next_step()
-            self.view.reset_button_clicks()
-    
+            self.view.reset_click_states() 
+        
     def toggle_pause(self):
         self.paused = not self.paused
 
@@ -57,13 +62,14 @@ class GridController:
         return 0 <= i < self.model.size and 0 <= j < self.model.size
     
     def update_widget_view(self):
-        self.view.set_turn(self.model.turn)
-        self.view.set_count(self.model.count_rabbits, self.model.count_foxes, self.model.count_carrots)
-        
-        self.model.set_speed(self.view.slider_speed.getValue())
-        self.model.set_smart_level(self.view.slider_smart_rabbit.getValue(), self.view.slider_smart_fox.getValue())
-        
-        
+        self.view.turn = self.model.turn
+        self.view.count_rabbit = self.model.count_rabbits
+        self.view.count_fox = self.model.count_foxes
+        self.view.count_carrot = self.model.count_carrots
+
+        self.model.speed = self.view.get_slider_value('speed')
+        self.model.smart_level_rabbit = self.view.get_slider_value('smart_rabbit')
+        self.model.smart_level_fox = self.view.get_slider_value('smart_fox')
 
     def render(self):
         

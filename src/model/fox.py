d3f502789654dc93971cc469aa2637b692dd3247
mnietona <Matias.nieto.navarrete@ulb.be>
2024-02-28 10:11:01 +0100

    Ajout faim

--- src/model/fox.py
+++ src/model/fox.py
@@ -8,11 +8,21 @@ class Fox(Fauna):
         self.target_type = "Rabbit"
         self.smart_level = smart_level
         self.adjust_radius_based_on_intelligence()  
+        self.hunger_level = 0  # Niveau de faim initial
+        self.hunger_threshold = 5  
     
     def update(self, i, j, grid):
+        self.hunger_level += 1
         super().update(i, j, grid)
         if self.age % 10 == 0:
             self.try_reproduce(grid)
+    
+    def eat_if_possible(self, position, grid):
+        if self.hunger_level >= self.hunger_threshold:
+            entity_at_new_position = grid.entity_positions.get(position)
+            if entity_at_new_position and isinstance(entity_at_new_position, grid.get_entity(self.target_type)):
+                self.health_level += entity_at_new_position.health_level
+                self.hunger_level = 0
 
     @property
     def color(self):
